0ed20a09a7506c2d6a5131d343f42926d23501f9
Zhuwangkun <74392390+Zhuwangkun@users.noreply.github.com>
2021-09-09 09:42:51 +0800

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,5 +8,6 @@ public class Test {
         System.out.println("hello shanghai");
         System.out.println("master");
         System.out.println("push");
+        System.out.println("pull");
     }
 }
